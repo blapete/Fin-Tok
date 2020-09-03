ea9d91cc9d478c5063125169f996c1e0a39f16fe
blapete <pjblank2@gmail.com>
2020-09-03 13:58:24 -0400

    salt

--- account/helper.js
+++ account/helper.js
@@ -1,7 +1,7 @@
 const SHA256 = require("crypto-js/sha256");
 const { APP_SECRET } = require("../secrets");
 
-// custom hash method to add to SHA256, makes our hash even more secure
+//salt
 const hash = (string) => {
   return SHA256(`${APP_SECRET}${string}${APP_SECRET}`).toString();
 };
