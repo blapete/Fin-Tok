d963cf6d7765482428c6d8a564e398b4c8b354ef
blapete <pjblank2@gmail.com>
2020-09-16 12:14:38 -0400

    heroku

--- backend/bin/server.js
+++ backend/bin/server.js
@@ -1,5 +1,50 @@
-const app = require("../app");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+// const morgan = require("morgan");
+const accountRouter = require("./api/account");
+const yahooRouter = require("./api/yahoo");
+const accountStocksRouter = require("./api/accountStocks");
 const db = require("../database/models");
+
+const app = express();
+
+//middleware
+// app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../../frontend/build"));
+} else {
+  app.use(express.static("../../frontend/public"));
+}
+
+//api routes
+app.use("/account", accountRouter);
+app.use("/stock", yahooRouter);
+app.use("/fav", accountStocksRouter);
+
+//error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    type: "error",
+    message: err.message,
+  });
+});
+
+//fallback
+app.get("*", (req, res) => {
+  if (process.env.NODE_ENV === "production") {
+    res.sendFile(path.resolve(__dirname, "../../frontend/build/index.html"));
+  } else {
+    res.sendFile(path.resolve(__dirname, "../../frontend/public/index.html"));
+  }
+});
 
 //port
 const port = process.env.PORT || 8080;
