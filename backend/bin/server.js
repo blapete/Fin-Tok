3aedfb570aa552254b18e1cf955e649dbff71924
blapete <pjblank2@gmail.com>
2020-09-17 00:58:46 -0400

    rearranged directory backend

---
+++ backend/bin/server.js
@@ -0,0 +1,7 @@
+const app = require("../app");
+const db = require("../database/models");
+const port = process.env.PORT || 8080;
+
+db.sequelize.sync().then(function () {
+  app.listen(port, () => console.log(`listening on port ${port}`));
+});
